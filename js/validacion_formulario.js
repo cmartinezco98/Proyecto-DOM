ecd95634e30f62195bf74e53831657867f2184a2
martinezco98 <cmartinezcodev@gmail.com>
2022-09-09 16:47:06 -0500

    Se agrega evento submit para gestion de envio de formulario

--- js/validacion_formulario.js
+++ js/validacion_formulario.js
@@ -1,18 +1,46 @@
 const d = document,
-w = window;
+  w = window;
 
-export default function validacionFormulario(){
-const $inputs = d.querySelectorAll(".contact-form [required]"),
-$form = d.querySelector(".contact-form");
+export default function validacionFormulario() {
 
-$inputs.forEach((input)=>{
+  const $inputs = d.querySelectorAll(".contact-form [required]"),
+    $form = d.querySelector(".contact-form");
+
+  $inputs.forEach((input) => {
     const $span = d.createElement("span");
     $span.id = input.name;
     $span.textContent = input.title;
-    $span.classList.add("contact-form-error","none");
-    input.insertAdjacentElement("afterend",$span);
-}
-)
+    $span.classList.add("contact-form-error", "none");
+    input.insertAdjacentElement("afterend", $span);
+  });
 
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(".contact-form [required]")) {
+      let $input = e.target,
+        pattern = $input.pattern || $input.dataset.pattern,
+        regExp = new RegExp(pattern);
+
+      if (pattern && $input.value !== "") {
+        return !regExp.exec($input.value)
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+      if (!pattern) {
+        $input.value == ""
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+    }
+  });
+
+
+  //Envio de formulario 
+  d.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+    alert("Enviado formulario");
+
+  });
 
 }
